1ee1644d807e9fd7e356cb7a2288761f457ef261
Debashrita Sahoo <138135798+debashritaSahoo12@users.noreply.github.com>
2025-03-30 13:04:12 +0530

    Update Complex_Websites.js

--- Complex_Websites.js
+++ Complex_Websites.js
@@ -3,7 +3,7 @@ fetch("https://jsonplaceholder.typicode.com/users")
   .then((data) => {
     function findComplexWebsiteUsers() {
       let complexWebUsers = data.filter(
-        (user) => user.website.split(".")[1].length >= 3
+        (user) => user.website.split(".")[0].length >= 3
       );
       console.log(`Users with complex websites: ${findComplexWebsiteUsers()}`);
       return complexWebUsers;
